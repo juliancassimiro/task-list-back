a96b50aa4ccd824e705dcdd0859632b21dfc9904
Julian Cassimiro <julian.cassimiro@outlook.com>
2024-08-02 15:41:13 -0300

    Corrigir tipo de dado no get-set

--- src/main/java/br/dev/jc/challenge/model/Task.java
+++ src/main/java/br/dev/jc/challenge/model/Task.java
@@ -29,11 +29,11 @@ public class Task {
     private Status status;
 
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
